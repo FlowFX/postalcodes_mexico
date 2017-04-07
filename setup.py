1c9de500fd97dbdb70664af09abcae68c3184365
Florian Posdziech <hallo@flowfx.de>
2017-04-06 19:48:21 -0500

    Improve tox testing

--- setup.py
+++ setup.py
@@ -40,9 +40,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
