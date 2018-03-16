728820e7d610a58ef7f497f604b931abf1f82fed
Florian Posdziech <hallo@flowfx.de>
2018-03-16 10:20:35 -0600

    Update cli test
    because now it actually tests some real output.

--- tests/test_postalcodes_mexico.py
+++ tests/test_postalcodes_mexico.py
@@ -76,9 +76,9 @@ class TestPlaces:
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.main, ['01000'])
     assert result.exit_code == 0
-    assert 'postalcodes_mexico.cli.main' in result.output
+    assert 'San Ángel' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
