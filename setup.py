17d103bc0098d8ec3524e09696143f8cb8bb5dd3
Florian Posdziech <hallo@flowfx.de>
2018-03-16 09:51:25 -0600

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/flowfx/postalcodes_mexico',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
