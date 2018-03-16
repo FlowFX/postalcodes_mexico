4214331aa408a84c2e4535197f6ccf4b0ff9628a
Florian Posdziech <hallo@flowfx.de>
2018-03-16 12:53:45 -0600

    Re-add utility functions
    Close: #82

---
+++ postalcodes_mexico/utils.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""Utils module of 'postalcodes_mexico'."""
+import sqlite3
+
+import xmltodict
+
+
+def update_db(db, xml_file):
+    """Update the places database.
+
+    Use the XML files provided by Correos Mexico.
+    """
+    places = xmltolist(xml_file)
+    con = sqlite3.connect(db)
+
+    with con:
+        con.execute("DROP TABLE IF EXISTS places")
+
+        sql = ("CREATE TABLE places("
+               "cp, place, place_type, municipality, city,  state)")
+        con.execute(sql)
+
+        sql = ("INSERT INTO places("
+               "cp, place, place_type, municipality, city, state) "
+               "VALUES (?, ?, ?, ?, ?, ?)")
+        con.executemany(sql, places)
+
+
+def xmltolist(xml_file):
+    """Convert the original XML file to a list of places as tuples."""
+    with open(xml_file, 'r') as f:
+        xml = f.read()
+
+        data = xmltodict.parse(xml, process_namespaces=True)
+        clean_data = data['NewDataSet']['NewDataSet:table']
+
+    # Some places don't have e.g. a municipality.
+    places = [(catch(lambda: row['NewDataSet:d_codigo']),
+               catch(lambda: row['NewDataSet:d_asenta']),
+               catch(lambda: row['NewDataSet:d_tipo_asenta']),
+               catch(lambda: row['NewDataSet:D_mnpio']),
+               catch(lambda: row['NewDataSet:d_ciudad']),
+               catch(lambda: row['NewDataSet:d_estado']),
+               ) for row in clean_data]
+
+    return places
+
+
+def catch(func, handle=lambda e: e, *args, **kwargs):
+    """Helper function to enable the list comprehension in xmltolist.
+
+    Whenenver a data field doesn't have an entry, put an empty string.
+
+    cf. http://stackoverflow.com/a/8915613
+    """
+    try:
+        return func(*args, **kwargs)
+    except KeyError:
+        return ''
---
+++ tests/test_utils.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `postalcodes_mexico.utils` module."""
+
+import sqlite3
+
+from postalcodes_mexico import postalcodes_mexico, utils
+
+
+def test_update_db():
+    # GIVEN a test database and a short XML file
+    test_db = 'test.db'
+    xml_file = 'tests/data/ciudad_de_mexico.xml'
+
+    # WHEN calling the database update function
+    utils.update_db(db=test_db, xml_file=xml_file)
+
+    con = sqlite3.connect(test_db)
+    con.row_factory = sqlite3.Row
+
+    # THEN the test database has entries
+    CP = '01000'
+    places = postalcodes_mexico.places(CP, db=test_db)
+
+    assert len(places) == 1
+    assert isinstance(places, list)
+
+    place = places[0]
+    assert place.postal_code == '01000'
+    assert place.place == 'San Ángel'
+    assert place.place_type == 'Colonia'
+    assert place.municipality == 'Álvaro Obregón'
+    assert place.city == 'Ciudad de México'
+    assert place.state == 'Ciudad de México'
+
+
+def test_xmltolist():
+    # GIVEN a short XML file
+    xml_file = 'tests/data/ciudad_de_mexico.xml'
+
+    # WHEN calling the xmltolist method
+    result = utils.xmltolist(xml_file=xml_file)
+
+    # THEN it returns a list with a lot of places
+    assert isinstance(result, list)
+    assert len(result) > 1000
+
+    # AND they are tuples
+    assert isinstance(result[5], tuple)
+
+
+def test_catch():
+    # GIVEN a dictionary
+    data = {'one': 'uno', 'two': 'dos'}
+
+    # WHEN using catch and existing dict key
+    # THEN it returns the value
+    assert utils.catch(lambda: data['one']) == 'uno'
+
+    # WHEN using catch and a non-existing dict key
+    # THEN it returns an empty string
+    assert utils.catch(lambda: data['three']) == ''
